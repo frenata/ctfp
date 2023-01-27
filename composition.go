a699cb91cf0ee7c56ce079b157d94c9a9ee34fe7
Andrew Nichols <andrew.nichols@debtsy.co>
2023-01-27 12:20:22 -0500

    Add memoize

--- composition.go
+++ composition.go
@@ -20,6 +20,10 @@ type Funky [A any, B any] struct {
 }
 //func (f funky[A, B]) Do(a A) B { return f.do(a) }
 
+func ID [A any] (a A) A {
+	return a
+}
+
 // Almost unreadable but surprisingly it works!
 // composes *f* after *g*
 func comp[A any, B any, C any](f Funky[B, C], g Funky[A, B]) Funky[A, C] {
@@ -32,10 +36,12 @@ func main() {
 	isEven := Funky[int, bool] {Do: func(n int) bool { return n%2 == 0 }}
 	constant := Funky[bool, string] {Do: func(b bool) string { return "const" }}
 	count := Funky[string, int] {Do: func(s string) int { return len(s) }}
+	intID := Funky[int, int]{Do: ID[int]}
 
 	fmt.Println(add1.Do(2))
 	fmt.Println(isEven.Do(2))
 	fmt.Println(comp(isEven,add1).Do(2))
 	fmt.Println(comp(constant, comp(isEven,add1)).Do(2))
 	fmt.Println(comp(count, comp(constant, comp(isEven,add1))).Do(2))
+	fmt.Println(comp(intID, comp(count, comp(constant, comp(isEven,comp(add1, intID))))).Do(2))
 }
---
+++ memoize.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+type Funky [A any, B any] struct {
+	Do func(a A) B
+}
+
+
+func fib(n int) int {
+	switch n {
+	case 1:
+		return 1
+	case 2:
+		return 1
+	default:
+		return fib(n-1) + fib(n-2)
+	}
+}
+
+type Cache[A comparable, B any] struct {
+	cache map[A]B
+}
+func NewCache[A comparable, B any]() *Cache[A, B] {
+	return &Cache[A, B]{ cache: make(map[A]B,0) }
+}
+func (c *Cache[A, B]) Memoize(f Funky[A, B]) Funky[A, B] {
+	g := func(a A) B {
+		if v, ok := c.cache[a]; ok {
+			return v
+		}
+		b := f.Do(a)
+		c.cache[a] = b
+		return b
+	}
+	return Funky[A, B] { Do: g}
+}
+
+func main() {
+	fibF := Funky[int, int] { Do: fib }
+	fibMemo := NewCache[int,int]().Memoize(fibF)
+	fmt.Println(fibMemo.Do(50))
+	fmt.Println(fibMemo.Do(50))
+}
