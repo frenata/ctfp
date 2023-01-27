88825f4ccb1998ca2803e8a5d0fbd15c87a30d8d
Andrew Nichols <andrew.nichols@debtsy.co>
2023-01-27 11:15:39 -0500

    Implement comp in go

---
+++ 1/composition.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+// but since you can't anonymously fulfill an interface! we also need a
+// struct that can take the composable functions
+/*type Funkifier[A any, B any] interface {
+	Do(A) B
+}*/
+
+// type parameters have to be associated with type *names*, so 
+// it doesn't seem possible to have an generic function as a parameter
+// aka something like 
+// f func[A, B](a A) B
+// 
+// to get around this problem, we have to wrap up our composable functions
+// into something else
+type Funky [A any, B any] struct {
+	Do func(a A) B
+}
+//func (f funky[A, B]) Do(a A) B { return f.do(a) }
+
+// Almost unreadable but surprisingly it works!
+// composes *f* after *g*
+func comp[A any, B any, C any](f Funky[B, C], g Funky[A, B]) Funky[A, C] {
+	// using Funkifier instead of Funky doesn't work due to failures of inference
+	return Funky[A, C]{ Do: func(a A) C { return f.Do(g.Do(a)) } }
+}
+
+func main() {
+	add1 := Funky[int, int]  {Do: func(n int) int { return n+1 }}
+	isEven := Funky[int, bool] {Do: func(n int) bool { return n%2 == 0 }}
+	constant := Funky[bool, string] {Do: func(b bool) string { return "const" }}
+	count := Funky[string, int] {Do: func(s string) int { return len(s) }}
+
+	fmt.Println(add1.Do(2))
+	fmt.Println(isEven.Do(2))
+	fmt.Println(comp(isEven,add1).Do(2))
+	fmt.Println(comp(constant, comp(isEven,add1)).Do(2))
+	fmt.Println(comp(count, comp(constant, comp(isEven,add1))).Do(2))
+}
